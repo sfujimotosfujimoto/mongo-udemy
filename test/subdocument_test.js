e50d35532f8252c52ecd5a776d4512371ee4fab5
S Fujimoto <unicoshun@gmail.com>
2016-12-15 07:33:58 +0900

    ~#53

--- test/subdocument_test.js
+++ test/subdocument_test.js
@@ -13,7 +13,46 @@ describe('Subdocuments', () => {
       .then((user) => {
         assert(user.posts[0].title === 'PostTitle');
         done();
-      }); 
+      });
 
+  });
+
+  it('can add subdocuments to an existing record', (done) => {
+    const joe = new User({
+      name: 'Joe',
+      posts: []
+    });
+
+    joe.save()
+      .then(() => User.findOne({ name: 'Joe' }))
+      .then((user) => {
+        user.posts.push({ title: 'New Post'});
+        return user.save();
+      })
+      .then(() => User.findOne({ name: 'Joe'}))
+      .then((user) => {
+        assert(user.posts[0].title === 'New Post');
+        done();
+      })
+  });
+
+  it('can remove an existing subdocument', (done) => {
+    const joe = new User({
+      name: 'Joe',
+      posts: [{ title: 'New Title'}]
+    });
+
+    joe.save()
+      .then(() => User.findOne({ name: 'Joe'}))
+      .then((user) => {
+        const post = user.posts[0]; //mongoose magic
+        post.remove();//mongoose magic
+        return user.save();
+      })
+      .then(() => User.findOne({ name: 'Joe'}))
+      .then((user) => {
+        assert(user.posts.length === 0);
+        done();
+      });
   });
 })
